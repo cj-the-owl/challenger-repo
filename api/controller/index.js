748fe9b5997194becab0a24d6e233d1e4e736361
cj-the-owl <lodewykcarynjayn@gmail.com>
2023-08-16 16:25:53 +0200

    books not done

--- api/controller/index.js
+++ api/controller/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = express.Router()
 //import models objects
-const {users} = require('../model') 
+const {users} = require('../model')
+const {books} = require('../model')
 
 // Users router
 routes.get('/users', (req, res) => {
@@ -22,6 +23,17 @@ routes.patch('/user/:id', bodyParser.json(), (req, res) => {
 })
 routes.delete('/user/:id', (req, res) => {
     users.deleteUser(req, res)
+})
+
+//Books router
+routes.get('/books', (req, res) => {
+    books.fetchBooks(req, res)
+})
+routes.get('/book/:id', (req, res) => {
+    books.fetchBooks(req, res)
+})
+routes.post('/book', bodyParser.json(), (req, res) => {
+    books.book(req, res)
 })
 
 module.exports = {
--- api/model/index.js
+++ api/model/index.js
@@ -1,5 +1,8 @@
 const Users = require('./users')
 
+const Books = require('./books')
+
 module.exports = {
     users: new Users(),
+    books: new Books()
 }
